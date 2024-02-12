319fc830279d8962540ea23862cd400cb99d9807
geatrigger-kim <geatrigger.kim@kakaocorp.com>
2024-02-13 08:56:13 +0900

    Do not change '-asdf' to '*asdf'

--- typora_markdown_to_confluence.bash
+++ typora_markdown_to_confluence.bash
@@ -4,14 +4,16 @@ filename="$1"
 
 # change bullet point
 ## print file
+## (  ) -> ()
 ## \n\n- -> \n*
 ## \n\n* -> \n*
 ## \n\n``` -> \n```
 ## h4, h3, h2, h1
 ## (  *) -> ** (max ****)
 cat $filename |
-sed -e ':a' -e 'N' -e '$!ba' -E -e 's/\n+( *)-/\n\1\*/g' |
-sed -e ':a' -e 'N' -e '$!ba' -E -e 's/\n+( *)\*/\n\1\*/g' |
+sed -E -e 's/^( )+$//g' |
+sed -e ':a' -e 'N' -e '$!ba' -E -e 's/\n+( *)- /\n\1\* /g' |
+sed -e ':a' -e 'N' -e '$!ba' -E -e 's/\n+( *)\* /\n\1\* /g' |
 sed -e ':a' -e 'N' -e '$!ba' -E -e 's/\n+( *)```/\n\1```/g' |
 sed -E -e 's/^#### /h4. /g' |
 sed -E -e 's/^### /h3. /g' |
@@ -19,9 +21,9 @@ sed -E -e 's/^## /h2. /g' |
 sed -E -e 's/^# /h1. /g' |
 sed -E -e 's/```$/\{code\}/g' |
 sed -E -e 's/```([a-z]*)/\{code:\1\}/g' |
-sed -E -e 's/^  \*/\*\*/g' |
-sed -E -e 's/^  \*/\*\*/g' |
-sed -E -e 's/^  \*/\*\*/g' |
-sed -E -e 's/^  \*/\*\*/g' 
+sed -E -e 's/^( *)  \*(\** )/\1\*\*\2/g' |
+sed -E -e 's/^( *)  \*(\** )/\1\*\*\2/g' |
+sed -E -e 's/^( *)  \*(\** )/\1\*\*\2/g' |
+sed -E -e 's/^( *)  \*(\** )/\1\*\*\2/g'
 
 # 
